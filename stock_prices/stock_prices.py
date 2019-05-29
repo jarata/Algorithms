0cea0f299f0c37daaa8c09a9f1fc626ac734d086
J A <jarata@gmail.com>
2019-05-28 20:36:43 -0700

    initial commit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,15 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
-
+# need a high an a low for a sale
+# buy before sell
+# find the max difference = high - low
+#
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+	# This is just some code to accept inputs from the command line
+	parser = argparse.ArgumentParser(description='Find max profit from prices.')
+	parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+	args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+	print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
