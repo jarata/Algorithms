df63f76016337b24b78f710802304c6fe2715609
J A <jarata@gmail.com>
2019-05-30 14:37:05 -0700

    testing small passed, working on refactoring for large

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,12 +5,22 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
+# How many way are there to eat 1 cookie more than once?
 def eating_cookies(n, cache=None):
-  pass
+	if n < 0:
+		return 0
+	elif n == 0:
+		return 1
+	else:
+	# 3 recursive calls
+		return eating_cookies(n - 1) + \
+		       eating_cookies(n - 2) + \
+		       eating_cookies(n - 3)
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+	if len(sys.argv) > 1:
+		num_cookies = int(sys.argv[1])
+		print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+	else:
+		print('Usage: eating_cookies.py [num_cookies]')
