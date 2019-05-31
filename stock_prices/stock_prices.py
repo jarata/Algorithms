9d06715058b2241732bc6a44c10d5f16745fa3fd
J A <jarata@gmail.com>
2019-05-30 19:16:42 -0700

    working on stock prices, testing returns error

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -6,7 +6,18 @@ def find_max_profit(prices):
 # need a high an a low for a sale
 # buy before sell
 # find the max difference = high - low
-#
+	max = None
+	low = prices[0]
+	high = prices[0]
+	for p in prices:
+		if p > low:
+			low = p
+			high = None
+		if high is None or p > high:
+			high = p
+		if max is None or high - low > max:
+			max = high - low
+	return max
 
 if __name__ == '__main__':
 	# This is just some code to accept inputs from the command line
